bc7c99cccf44d7ca2037026500d14194023c0529
cosic04 <palacinke456>
2021-08-18 15:26:29 +0200

    Written tests for DataDetailsServiceImpl.

---
+++ src/test/java/com/apisaes/task/services/DataDetailsServiceImplTest.java
@@ -0,0 +1,86 @@
+package com.apisaes.task.services;
+
+import com.apisaes.task.repository.DataDetailsRepository;
+import hr.aaa.test.v0.datadetails.DataDetails;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.xml.crypto.Data;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class DataDetailsServiceImplTest {
+
+    @Mock
+    DataDetailsRepository dataDetailsRepository;
+
+    DataDetailsService dataDetailsService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        dataDetailsService = new DataDetailsServiceImpl(dataDetailsRepository);
+    }
+
+    @Test
+    void findAllDataDetails() {
+        when(dataDetailsRepository.findAllDataDetails()).thenReturn(
+                        Arrays.asList(new DataDetails(), new DataDetails())
+        );
+
+        List<DataDetails> list = dataDetailsService.findAllDataDetails();
+
+        verify(dataDetailsRepository, times(1)).findAllDataDetails();
+        assertNotNull(list);
+        assertEquals(2, list.size());
+        assertNotEquals(0, list.size());
+    }
+
+    @Test
+    void findById() {
+        DataDetails defaultDataDetails = new DataDetails();
+
+        when(dataDetailsRepository.findById(any())).thenReturn(
+                new DataDetails()
+        );
+
+        DataDetails dataDetails = dataDetailsService.findById("1");
+
+        verify(dataDetailsRepository, times(1)).findById(any());
+        assertNotNull(dataDetails);
+    }
+
+    @Test
+    void findByOperationId() {
+        when(dataDetailsRepository.findAllByOperationId(any())).thenReturn(
+                Arrays.asList(new DataDetails(), new DataDetails())
+        );
+
+        List<DataDetails> list = dataDetailsService.findByOperationId("2");
+
+        verify(dataDetailsRepository, times(1)).findAllByOperationId(any());
+        assertNotNull(list);
+        assertEquals(2, list.size());
+        assertNotEquals(0, list.size());
+    }
+
+    @Test
+    void saveDataDetails() {
+        DataDetails givenDataDetail = new DataDetails();
+        when(dataDetailsRepository.saveDataDetails(any())).thenReturn(
+                givenDataDetail
+        );
+
+        DataDetails dataDetails = dataDetailsService.saveDataDetails(givenDataDetail);
+
+        verify(dataDetailsRepository, times(1)).saveDataDetails(any());
+        assertNotNull(dataDetails);
+        assertEquals(dataDetails, givenDataDetail);
+    }
+}
