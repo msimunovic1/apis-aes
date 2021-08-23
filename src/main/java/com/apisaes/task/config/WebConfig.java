7cf6b0867623fbac1ad93be3306afb51a1ba329d
cosic04 <palacinke456>
2021-08-23 13:15:55 +0200

    Removed a view dataDetails.html because it hasn't been used. Added class that loads hardcoded data at the start of the application. Removed annotation @EnableWebMvc from WebConfig because it has been decided to leave the framework responsible for setting up an MVC part of the application.

--- src/main/java/com/apisaes/task/config/WebConfig.java
+++ src/main/java/com/apisaes/task/config/WebConfig.java
@@ -7,20 +7,12 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@EnableWebMvc
-//@Configuration
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         //WebMvcConfigurer.super.configureContentNegotiation(configurer);
         configurer.defaultContentType(MediaType.APPLICATION_XML);
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/");
     }
 }
