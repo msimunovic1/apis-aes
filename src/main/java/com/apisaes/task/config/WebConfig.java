62fcb4948a1c732c0a601a0a7e1151b787a40926
msimunovic1 <mateasimunovic01@gmail.com>
2021-08-23 12:37:33 +0200

    Removed @EnableWebMvc annotation from WebConfig file

--- src/main/java/com/apisaes/task/config/WebConfig.java
+++ src/main/java/com/apisaes/task/config/WebConfig.java
@@ -3,24 +3,14 @@ package com.apisaes.task.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@EnableWebMvc
-//@Configuration
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        //WebMvcConfigurer.super.configureContentNegotiation(configurer);
         configurer.defaultContentType(MediaType.APPLICATION_XML);
     }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/");
-    }
 }
+
